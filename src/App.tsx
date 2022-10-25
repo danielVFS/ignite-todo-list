7c65552424aea41e7063da7ef1fe92bccd8e5266
danielVFS <danielv759153@gmail.com>
2022-10-25 16:45:17 -0300

    feat(NewTask): create base component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { Header } from "./components/Header";
+import { ListTasks } from "./components/ListTasks";
+import { NewTask } from "./components/NewTask";
 import "./styles/global.scss";
 
 export function App() {
   return (
     <>
       <Header />
+      <NewTask />
+      <ListTasks />
     </>
   );
 }
--- src/components/NewTask/index.tsx
+++ src/components/NewTask/index.tsx
@@ -1,5 +1,10 @@
 import styles from "./NewTask.module.scss";
 
 export function NewTask() {
-  return <div className={styles.newTask}></div>;
+  return (
+    <div className={styles.newTask}>
+      <input placeholder="Adicionar uma nova tarefa" />
+      <button>Criar</button>
+    </div>
+  );
 }
