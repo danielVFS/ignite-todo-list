46ff7325f391ce531f50ba734275035b83a40477
danielVFS <danielv759153@gmail.com>
2022-10-25 17:22:14 -0300

    feat(NewTask): create component

--- src/components/NewTask/index.tsx
+++ src/components/NewTask/index.tsx
@@ -1,10 +1,13 @@
+import { PlusCircle } from "phosphor-react";
 import styles from "./NewTask.module.scss";
 
 export function NewTask() {
   return (
     <div className={styles.newTask}>
       <input placeholder="Adicionar uma nova tarefa" />
-      <button>Criar</button>
+      <button>
+        Criar <PlusCircle size={22} />
+      </button>
     </div>
   );
 }
