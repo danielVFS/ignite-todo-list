73070e3de2def7146ff867b803909c187b398038
danielVFS <danielv759153@gmail.com>
2022-10-25 16:52:32 -0300

    feat(ListTasks): base component

--- src/components/ListTasks/index.tsx
+++ src/components/ListTasks/index.tsx
@@ -1,5 +1,37 @@
+import { Trash } from "phosphor-react";
+import { tasks } from "../../core/data/tasks.mock";
 import styles from "./ListTasks.module.scss";
 
 export function ListTasks() {
-  return <div className={styles.listTasks}></div>;
+  return (
+    <div className={styles.listTasks}>
+      <header>
+        <div>
+          <p>Tarefas Criadas</p>
+          <span>5</span>
+        </div>
+        <div>
+          <p>Concluídas</p>
+          <span>2 de 5</span>
+        </div>
+      </header>
+      <section>
+        {tasks.map((task) => {
+          return (
+            <div key={task.id}>
+              <div>
+                <input
+                  type="checkbox"
+                  name="readTask"
+                  checked={task.isCompleted}
+                />
+                <p>{task.text}</p>
+              </div>
+              <Trash />
+            </div>
+          );
+        })}
+      </section>
+    </div>
+  );
 }
