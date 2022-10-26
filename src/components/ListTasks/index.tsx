ce1242e2ced7acab2efa17466c83f0f74b1c5c75
danielVFS <danielv759153@gmail.com>
2022-10-26 17:08:38 -0300

    feat(Tasks): count completed tasks

--- src/components/ListTasks/index.tsx
+++ src/components/ListTasks/index.tsx
@@ -1,18 +1,29 @@
-import { tasks } from "../../core/data/tasks.mock";
+import { useState } from "react";
+import { tasks as tasksMock } from "../../core/data/tasks.mock";
+import { Task as TaskModel } from "../../core/models/Task";
 import { Task } from "../Task";
 import styles from "./ListTasks.module.scss";
 
 export function ListTasks() {
+  const [tasks, setTasks] = useState<TaskModel[]>(tasksMock);
+
+  const completedTasks = tasks.reduce(
+    (prev, current) => (current.isCompleted ? prev + 1 : prev),
+    0
+  );
+
   return (
     <div className={styles.listTasks}>
       <header className={styles.header}>
         <div>
           <p className={styles.createdTasks}>Tarefas Criadas</p>
-          <span className={styles.badge}>5</span>
+          <span className={styles.badge}>{tasks.length}</span>
         </div>
         <div>
           <p className={styles.concludedTasks}>Concluídas</p>
-          <span className={styles.badge}>2 de 5</span>
+          <span className={styles.badge}>
+            {completedTasks} de {tasks.length}
+          </span>
         </div>
       </header>
       <section className={styles.tasks}>
