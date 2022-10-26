d3d949b534bfc2f47dc06edc065890b565288fc9
danielVFS <danielv759153@gmail.com>
2022-10-26 20:06:41 -0300

    feat(TasksList): empty tasks screen

--- src/components/ListTasks/index.tsx
+++ src/components/ListTasks/index.tsx
@@ -1,3 +1,4 @@
+import { ListPlus } from "phosphor-react";
 import { Task as TaskModel } from "../../core/models/Task";
 import { Task } from "../Task";
 import styles from "./ListTasks.module.scss";
@@ -32,22 +33,34 @@ export function ListTasks({
           </span>
         </div>
       </header>
-      <section className={styles.tasks}>
-        {tasks
-          .sort((a, b) => Number(a.isCompleted) - Number(b.isCompleted))
-          .map((task) => {
-            return (
-              <Task
-                key={task.id}
-                id={task.id}
-                isCompleted={task.isCompleted}
-                text={task.text}
-                onCompletedTask={onCompleteTask}
-                onDeleteTask={onDeleteTask}
-              />
-            );
-          })}
-      </section>
+      {tasks.length > 0 ? (
+        <section className={styles.tasks}>
+          {tasks
+            .sort((a, b) => Number(a.isCompleted) - Number(b.isCompleted))
+            .map((task) => {
+              return (
+                <Task
+                  key={task.id}
+                  id={task.id}
+                  isCompleted={task.isCompleted}
+                  text={task.text}
+                  onCompletedTask={onCompleteTask}
+                  onDeleteTask={onDeleteTask}
+                />
+              );
+            })}
+        </section>
+      ) : (
+        <div className={styles.emptyList}>
+          <ListPlus size={56} />
+          <div className={styles.info}>
+            <h4 className={styles.title}>
+              Você ainda não tem tarefas cadastradas
+            </h4>
+            <span>Crie tarefas e organize seus itens a fazer</span>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
